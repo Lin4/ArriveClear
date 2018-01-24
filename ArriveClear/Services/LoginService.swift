865c40600f5cc9fa499fd6c3666f6626f6f96dc9
Lin4 <lingesus@gmail.com>
2018-01-24 10:58:47 -0500

    initial commit

---
+++ ArriveClear/Services/LoginService.swift
@@ -0,0 +1,9 @@
+//
+//  LoginService.swift
+//  ArriveClear
+//
+//  Created by Lingeswaran Kandasamy on 1/24/18.
+//  Copyright © 2018 Lingeswaran Kandasamy. All rights reserved.
+//
+
+import Foundation
